f56df3387b89558338877126a3963b3e834a6487
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-27 14:29:12 -0700

    add function to get movie title results fixed

--- src/main.js
+++ src/main.js
@@ -4,43 +4,35 @@ import './styles.css';
 import $ from 'jquery';
 import { Movies } from './movies-older.js';
 
-$(document).ready(function() {
-  $('#movie-title').click(function() {
+$(document).ready(function () {
+  $('#movie-title').click(function () {
     let title = $('#movie').val();
     $('#movie').val("");
 
     (async () => {
       let movieTitle = new Movies();
       const response = await movieTitle.getMoviebyTitle(title);
-      getElements(response,title);
+      getElements(response, title);
       console.log(response.results);
     })();
 
     function getElements(response, title) {
-      console.log(response, title)
+      console.log(response,title);
       if (response) {
-        console.log(response, title)
-        for (let i = 0; i<response.results.length; i++){
-          if(response.results.includes(title)){
-            console.log(response.results[i].original_title + " --------");
-            $('#results').append(`${response.results[0].original_title}`);
-          }
-        } 
-       } else {
+        console.log(response,title);
+       
+        for (let i = 0; i < response.results.length; i++) {
+          // if (response.results.includes(title)) {
+          // console.log("val");
+          // }
+          $('#results').append(`<li>${response.results[i].original_title}</li>`);
+        }
+      } else {
         $('#results').text(`There was an error handling your request.`);
-     
+
       }
     }
   });
 });
 
 
-// getTitle(respons,title){
-//   for(let i=0;i<respons.length;i++){
-//   if(respons.results[i].title === title){
-  
-//      return respons.results[i];
-//   }
-  
-//   }
-// }

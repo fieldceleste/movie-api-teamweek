adfabacff54167d141024b32b70310f3642f328c
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-27 16:47:57 -0700

    Add movie image API call function in movies-image.js and getMovieId function in main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 import { Movies } from './movies-older.js';
+// import { MovieImages } from './movies-image.js';
+
+
 
 $(document).ready(function () {
   $('#movie-title').click(function () {
@@ -13,6 +16,7 @@ $(document).ready(function () {
       let movieTitle = new Movies();
       const response = await movieTitle.getMoviebyTitle(title);
       getElements(response, title);
+      getMovieId(response); 
       console.log(response.results);
     })();
 
@@ -20,19 +24,48 @@ $(document).ready(function () {
       console.log(response,title);
       if (response) {
         console.log(response,title);
-       
         for (let i = 0; i < response.results.length; i++) {
           // if (response.results.includes(title)) {
           // console.log("val");
           // }
-          $('#results').append(`<li>${response.results[i].original_title}</li>`);
-          
+          $('#results').append(`<li>${response.results[i].original_title}</li>`);         
         }
       } else {
         $('#results').text(`There was an error handling your request.`);
-
       }
     }
+
+    function getMovieId (response){
+      if (response) {
+        console.log(response.results[0].id);
+      }
+    }
+
+
+// response.results[i].id = 234897654
+
+    // (async () => {
+    //   let movieImage = new MovieImages();
+    //   const response = await movieImage.getMovieImages(movieId);
+    //   getImg(response, movieId);
+    //   // console.log(response.movieId);
+    // })();
+    // function getImg(response, movieId) {
+    //   console.log(response,movieId);
+    //   if (response) {
+    //     console.log(response,movieId);
+    //     for (let i = 0; i < response.movieId.length; i++) {
+    //       // if (response.results.includes(title)) {
+    //       // console.log("val");
+    //       // }
+    //       $('#results').append(`<li>${response.posters}</li>`);         
+    //     }
+    //   } else {
+    //     $('#results').text(`There was an error handling your request.`);
+    //   }
+    // }
+
+
   });
 });
 

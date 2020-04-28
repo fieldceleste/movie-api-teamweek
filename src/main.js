c439a7fc71bf86d239b06b863f9d25f6c4f8668f
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-28 13:42:05 -0700

    add Img function and place it in getElements function to display images for each movie

--- src/main.js
+++ src/main.js
@@ -12,46 +12,54 @@ $(document).ready(function () {
     let title = $('#movie').val();
     let movieId = $("movie").val();
     $('#movie').val("");
-
+//----------Movie Title Function--------------------------------------------
     (async () => {
       let movieTitle = new Movies();
       const response = await movieTitle.getMoviebyTitle(title);
       getElements(response, title);
       movieIdArr(response);
       
-      console.log(movieId);
+      console.log(movieId); //---Undefined
     })();
 
     function getElements(response, title) {
-      console.log(title);
+      console.log(title); //-----Working
       if (response) {
         for (let i = 0; i < response.results.length; i++) {
           $('#results-title').append(`<li id =${response.results[i].id}>${response.results[i].original_title}</li>`);
-          $("#results-img").append(`<img src="" alt="photo here">`);
+          Img(response.results[i].id);
         }
       } else {
         $('#results').text(`There was an error handling your request.`);
       }
     }
+    //---------End of Movie Title-----------------------------------------------
     function movieIdArr(response) {
+      console.log(response); //---showing array of image details
       let Arr = [];
       if (response) {
         for (let i = 0; i < response.results.length; i++) {
           Arr.push(response.results[i].id);
+          // console.log(Arr);
         }
         return Arr;
       }
     }
 
-    (async () => {
-      let movieImg = new MovieImages();
-      const imageResponse = await movieImg.getMovieImage(684139);
-      dispImg(imageResponse);
-    })();
-    function dispImg(response) {
-      console.log(response);
-      if (response) {
-        $("#results-img").html(`<img src=${response.backdrops.posters} alt="photo here">`);
+    function Img (movieId){
+      (async () => {
+        let movieImg = new MovieImages();
+        const imageResponse = await movieImg.getMovieImage(movieId); //use movieIdArr to push each id in here
+        console.log(movieId);//------Undefined
+        // movieIdArr(imageResponse); //[346765, 2343453, 345234]  --maybe add movieId
+        dispImg(imageResponse);
+      })();
+  
+      function dispImg(response) {
+        if (response) {
+          console.log(response); //----Getting object  
+          $("#results-title").append(`<img src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${response.posters[0].file_path}">`);
+        }
       }
     }
 

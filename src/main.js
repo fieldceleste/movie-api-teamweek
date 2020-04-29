757e930c4d8b5706e177b40cb4649a31543dda0d
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-29 11:20:42 -0700

    fix changes from other project

--- src/main.js
+++ src/main.js
@@ -4,36 +4,120 @@ import './styles.css';
 import $ from 'jquery';
 import { Movies } from './movies-older.js';
 
-$(document).ready(function () {
+
+let movieObj;
+let currentMovie;
+$(document).ready(function(){
+  movieObj = new Movies();
+  attachMovieListeners();
+
+  $("#showFMovieList").click(function(){
+    showFavoriteMovieList(movieObj);
+    $("#fMovieList").toggle();
+  });
+  
+  //add to the favorite list
+  // $("#details").click(function(){
+  //   $("#details").hide();
+  //   //$("#fMovieList").show();
+  // });
+  $("#details").on("click", "button",function(){
+    $("#details").hide();
+    console.log(currentMovie)
+    movieObj.addfavoriteMovieList(currentMovie);
+  });
+
+// show movie list
   $('#movie-title').click(function () {
+    $("#details").html("");
+    $("#results").html("");
+    $("#fMovieList").html("");
+    
+    $("#results").show();
+    
     let title = $('#movie').val();
     $('#movie').val("");
-
+    
     (async () => {
-      let movieTitle = new Movies();
-      const response = await movieTitle.getMoviebyTitle(title);
-      getElements(response, title);
-      console.log(response.results);
+      //let movieTitle = new Movies();
+      const response = await movieObj.getMoviebyTitle(title);
+      getElements(response);
     })();
 
-    function getElements(response, title) {
-      console.log(response,title);
+    function getElements(response) {
       if (response) {
-        console.log(response,title);
-       
-        for (let i = 0; i < response.results.length; i++) {
-          // if (response.results.includes(title)) {
-          // console.log("val");
-          // }
-          $('#results').append(`<li>${response.results[i].original_title}</li>`);
-          
+        let htmlInfo;
+        for (let i = 0; i < response.results.length; i++) { 
+                                               
+          htmlInfo = `<div class="p-2 border d-flex flex-wrap align-content-center bg-light">
+                  <h5><a id="${response.results[i].id}" href="#">${response.results[i].original_title}</h5>
+                  <div class="card">    
+                    <p>Year :${response.results[i].release_date}</p>
+                    <img class="card-img-top" src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${response.results[i].poster_path}" style="width: 7rem" alt="Card image cap">
+                  </div></a>
+              </div>` 
+           $('#results').append(`${htmlInfo}`);
         }
       } else {
         $('#results').text(`There was an error handling your request.`);
-
       }
     }
   });
 });
+  
+//get movie details
+  function attachMovieListeners() {
+    
+  $(".movieList").on("click","a", function(event) {
+    $("#results").hide();
+    $("#fMovieList").hide();
+    $("#details").show();
+    event.preventDefault();
+    
+    (async () => {
+      const response = await movieObj.displayDetailPage(this.id);
+      getDetails(response);
+    })();
+
+    
+    function getDetails(response) {
+      // let movieInfo;
+      if (response) {
+        currentMovie = response;
+        console.log(currentMovie);
+        //movieObj.addfavoriteMovieList(response);
+        //showFavoriteMovieList(movieObj);
+        
+        let movieInfo =  `<div class="p-2 border d-flex flex-wrap align-content-center bg-light"><br>
+                            <div class="card">
+                              <h5>${response.original_title}</h5>   
+                              <p>Year :${response.release_date}</p>
+                              <img class="card-img-top" src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${response.poster_path}" style="width: 18rem" alt="Card image cap"/>
+                              <button type="button" class="btn btn-primary" id="favoriteMoveiList">Add To Your Favorite Movie List</button>        
+                              </div>
+                          </div>` 
+            $('#details').html(`${movieInfo}`);
+        } else {
+        $('#details').text(`There was an error handling your request.`);
+      }
+    }
+  })
+  }  
+
+  function showFavoriteMovieList(movieObj){
+    let movieListInfo="";
+    movieObj.favoriteMovieList.forEach(function(movie){
+      
+      movieListInfo =  `<div class="p-2 border d-flex flex-wrap align-content-center bg-light"><br>
+              <div class="card">
+                <h3> Favorite Movei List</h3>
+                <h5>${movie.original_title}</h5>   
+                <p>Year :${movie.release_date}</p>
+                <img class="card-img-top" src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${movie.poster_path}" style="width: 18rem" alt="Card image cap">
+              </div>
+            </div>` 
+          $("#fMovieList").append(movieListInfo);
+    });
+  }
 
 
--- src/movies-older.js
+++ src/movies-older.js
@@ -1,8 +1,17 @@
 export class Movies {
+  constructor(){
+    this.favoriteMovieList = [];
+  }
+  addfavoriteMovieList(response){
+    this.favoriteMovieList.push(response);
+  }
+
+  
+
   async getMoviebyTitle(title) {
     let jsonifiedResponse;
     try {
-      let response = await fetch(`https://api.themoviedb.org/3/search/movie/?api_key=${process.env.API_KEY}&query=${title}`);
+      let response = await fetch(`https://api.themoviedb.org/3/search/movie/?api_key=${process.env.api_key}&query=${title}`);
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {
@@ -13,23 +22,37 @@ export class Movies {
     }
     return jsonifiedResponse;
   }
+
+
+  async displayDetailPage(id) {
+    let jsonifiedResponse;
+    try {
+      let response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.api_key}`);
+   
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+    } catch (error) {
+      return false;
+    }
+    return jsonifiedResponse;
+  }
+  
+  // displayDetailPage() {
+
+    
+  //   if (htmlInfo === "none") {
+  //     htmlInfo = "block";
+  //   } else {
+  //     htmlInfo = "none";
+  //   }
+  // }
+  
 }
 
 
 
-//http://api.themoviedb.org/3/search/movie?api_key=ced83ccd2ee66a3e7d710304f25a076f&query=Fight%20Club
-
-// `https://api.themoviedb.org/3/search/movie/?${process.env.API_KEY}&query=${title}`);
-// https://api.themoviedb.org/3/search/movie/?api_key=ced83ccd2ee66a3e7d710304f25a076f&query=les miserables
-
-//https://api.themoviedb.org/3/movie/550?api_key=ced83ccd2ee66a3e7d710304f25a076f
-
-// https://api.themoviedb.org/3/search/movie/?api_key=ced83ccd2ee66a3e7d710304f25a076f&query=string  
-
-// https://api.themoviedb.org/3/search/movie/?api_key=ced83ccd2ee66a3e7d710304f25a076f&query=fight%club 
-
-// https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1
-
-// https://api.themoviedb.org/3/search/movie/?api_key=ced83ccd2ee66a3e7d710304f25a076f&query=avengers
 
 

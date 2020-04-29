e96624d2ccf0caaccb1af201b9620633828c6da0
nitunwa <nitun.2@gmail.com>
2020-04-28 17:01:54 -0700

    add get details from movie list

--- src/main.js
+++ src/main.js
@@ -4,38 +4,9 @@ import './styles.css';
 import $ from 'jquery';
 import { Movies } from './movies-older.js';
 
-$(document).ready(function () {
-
-// diplayDeatils
-  $("#result").click(function(){
-    let value = $("#result").val(); 
-
-    (async () => {
-      let movie = new Movies();
-      const response = await movie.displayDetailPage(value);
-      getElements(response);
-      console.log(response.results);
-    })();
-
-    function getElements(response) {
-      if (response) {
-        let htmlInfo;
-        for (let i = 0; i < response.results.length; i++) {
-          htmlInfo = `<div class="p-2 border d-flex flex-wrap align-content-center bg-light" ><h5><a href="https://www.themoviedb.org/movie/${response.results[i]}"><p>${response.results[i].original_title}</p></h5>
-                        <div class="card"">    
-                          <p>Year :${response.results[i].release_date}</p>
-                          <img class="card-img-top" src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${response.results[i].poster_path}" style="width: 7rem" alt="Card image cap">
-                        </div>
-                     </div></a>` 
-           $('#results').append(`${htmlInfo}`);
-        }
-      } else {
-        $('#results').text(`There was an error handling your request.`);
-      }
-    }
-  });
-//end display Details
-
+$(document).ready(function(){
+  console.log("value")
+  attachMovieListeners();
 // show movie list
   $('#movie-title').click(function () {
     let title = $('#movie').val();
@@ -47,17 +18,23 @@ $(document).ready(function () {
       getElements(response);
       console.log(response.results);
     })();
+
+    // htmlInfo = `<div class="movieDetails p-2 border d-flex flex-wrap align-content-center bg-light"><h5><a href="<p>${response.results[i].original_title}</p></h5>
+    
     
     function getElements(response) {
+      console.log('line 26 response: ', response)
       if (response) {
         let htmlInfo;
-        for (let i = 0; i < response.results.length; i++) {
-          htmlInfo = `<div class="p-2 border d-flex flex-wrap align-content-center bg-light"><h5><a href="https://www.themoviedb.org/movie/${response.results[i]}"><p>${response.results[i].original_title}</p></h5>
-                        <div class="card"">    
+        for (let i = 0; i < response.results.length; i++) {                                       
+          htmlInfo = `<div class="p-2 border d-flex flex-wrap align-content-center bg-light">
+                       <h5><span id="${response.results[i].id}">${response.results[i].original_title}</span></h5>
+                        <div class="card">    
                           <p>Year :${response.results[i].release_date}</p>
                           <img class="card-img-top" src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${response.results[i].poster_path}" style="width: 7rem" alt="Card image cap">
                         </div>
-                     </div></a>` 
+                  
+                     </div>` 
            $('#results').append(`${htmlInfo}`);
 
         }
@@ -67,6 +44,50 @@ $(document).ready(function () {
     }
   });
 
+
+
 });
+function attachMovieListeners() {
+   console.log("value2"); 
+  $(".movieList").on("click","span", function(event) {
+    event.preventDefault();
+    
+    (async () => {
+      let movieDetails = new Movies();
+      console.log("value1 " + this.id)
+      const response = await movieDetails.displayDetailPage(this.id);
+      
+      getDetails(response);
+      console.log(response.original_title + "  val1");
+    })();
 
+    //belongs_to_collection.name
 
+    // https://api.themoviedb.org/3/movie/24428?api_key=ced83ccd2ee66a3e7d710304f25a076f
+
+    function getDetails(response) {
+      if (response) {
+        
+        // for (let i = 0; i < response.results.length; i++) {                                       
+           let htmlInfo = `<div class="p-2 border d-flex flex-wrap align-content-center bg-light">
+                       <h5>${response.overview}">${response.original_title}</h5>
+                        <div class="card">    
+                          <p>Year :${response.release_date}</p>
+                          <img class="card-img-top" src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${response.poster_path}" style="width: 7rem" alt="Card image cap">
+                        </div>
+                  
+                     </div>` 
+           $('#results').append(`${htmlInfo}`);
+
+      // }
+      } else {
+        $('#results').text(`There was an error handling your request.`);
+      }
+    }
+   
+
+    
+  })
+  }
+
+//https://api.themoviedb.org/3/movie/${response.results[i].id}

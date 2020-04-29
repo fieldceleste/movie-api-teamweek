ca4846c8f863e2774c294a4f8034a76cda9d4168
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-28 17:03:10 -0700

    add alt messages for the image and add styles

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
   $('#movie-results').click(function () {
     let title = $('#movie').val();
     $('#movie').val("");
-
+    
     (async () => {
       let movieTitle = new Movies();
       const response = await movieTitle.getMoviebyTitle(title);
@@ -20,7 +20,7 @@ $(document).ready(function () {
       if (response) {
         const respResults = response.results;
         for (let i = 0; i < respResults.length; i++) {
-          $('#results-title').append(`<li id =${respResults[i].id}> <img src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${respResults[i].poster_path}"> ${respResults[i].original_title} </li>`);
+          $('#results-title').append(`<li id =${respResults[i].id}> <img src="https://image.tmdb.org/t/p/w94_and_h141_bestv2${respResults[i].poster_path}" alt="Sorry! No image of ${respResults[i].original_title}."> ${respResults[i].original_title} </li>`);
         }
       } else {
         $('#results').text(`There was an error handling your request.`);

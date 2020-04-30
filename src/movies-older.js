e6bc1ed85259a8ebeecedc98eaf8b2ef31d2f75b
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-29 17:01:54 -0700

    add in button changes and styling to css

--- src/movies-older.js
+++ src/movies-older.js
@@ -1,17 +1,17 @@
 export class Movies {
 
-  constructor(){
+  constructor() {
     this.favoriteMovieList = [];
   }
-  
-  addfavoriteMovieList(response){
+
+  addfavoriteMovieList(response) {
     this.favoriteMovieList.push(response);
   }
-
+  
   async getMoviebyTitle(title) {
     let jsonifiedResponse;
     try {
-      let response = await fetch(`https://api.themoviedb.org/3/search/movie/?api_key=${process.env.API_KEY}&query=${title}`); 
+      let response = await fetch(`https://api.themoviedb.org/3/search/movie/?api_key=${process.env.api_key}&query=${title}`);
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {
@@ -27,9 +27,8 @@ export class Movies {
   async displayDetailPage(id) {
     let jsonifiedResponse;
     try {
-      let response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`);
-      
-   
+      let response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.api_key}`);
+
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {

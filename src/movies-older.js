80fbdc7d571949061b78db34121b9f9d13d8f085
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-29 17:03:34 -0700

    add in button changes and styling to css

--- src/movies-older.js
+++ src/movies-older.js
@@ -41,3 +41,33 @@ export class Movies {
     return jsonifiedResponse;
   }
 }
+  constructor(){
+    this.favoriteMovieList = [];
+  }
+  
+  addfavoriteMovieList(response){
+    this.favoriteMovieList.push(response);
+  }
+
+  async getMoviebyTitle(title) {
+    let jsonifiedResponse;
+    try {
+      let response = await fetch(`https://api.themoviedb.org/3/search/movie/?api_key=${process.env.API_KEY}&query=${title}`); 
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+    } catch (error) {
+      return false;
+    }
+    return jsonifiedResponse;
+  }
+
+
+  async displayDetailPage(id) {
+    let jsonifiedResponse;
+    try {
+      let response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`);
+      
+   
